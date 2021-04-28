25f2bf85428d32ee50d3a056baafd609011b7bb3
“Kunwarpreet <Kunwarpreetbehar@gmail.com>
2021-04-28 16:43:13 -0700

    Updated color scheme

---
+++ One-SJ/src/components/pages/aboutUsPage/aboutUs.jsx
@@ -0,0 +1,31 @@
+//import background from "../../../img/Black.jpg";
+import NavigationBar from "../../reusable/navigationBar";
+import OneSJHeader from "../../reusable/onesjHeader";
+import AboutUsParagraph from "./aboutUsParagraph";
+import Footer from "../../reusable/footer";
+
+const AboutUs = () => {
+  return (
+    <>
+      <NavigationBar />
+      <div
+        style={{
+          //backgroundImage: `url(${background})`,
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          placeItems: "center",
+          background:
+            "linear-gradient(to bottom, rgb(0, 150, 140), rgb(250, 250, 210))",
+          height: "180vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <OneSJHeader />
+        <AboutUsParagraph />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AboutUs;
